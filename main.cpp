8b3307414542aaa2492a7383354ad159d3604a37
ogunley6 <ogunley6@uni.coventry.ac.uk>
2017-10-20 13:49:13 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,48 +1,59 @@
 #include <iostream>
-#include <cmath>
-#include <cstdlib>
 
 using namespace std;
 
-//two non negative integers m is the number that divides into n!
+int userValue;
+int au1();
+int au2();
+
 
 int main(){
-    int m;
-    int n;
-    unsigned long long factorial = 1;
 
-    cout<< "Enter 2 numbers (m and n) both can be any positive integer of your choice \n";
-    cout<< "The program will then run and see whether the first digit m divides into the second digit of your choice n!"<<endl;
+    int cu; //The maximum mass is 7kg
+    int plastic; //The maximum mass is 15kg
+    int au; //The maximum mass is 4kg
 
-    cout<<"Enter your first choice of number \n";
-    cin>>m;
+//The unit cost of each material
 
-    cout<<"Now enter your second \n";
-    cin>>n;
+    int uCopper = 65;
+    int uPlastic = 15;
+    int uGold = 100;
 
-    for(int i = 1; i <=n; ++i)
-    {
-        factorial *= i;
+    cout <<"Please enter a number no bigger than 26. \n";
+    cin >> userValue;
+
+//if statement to make sure user never goes above Maximum available mass.
+    if(userValue > 26){
+            cout <<"Sorry you can't input above the total of 26kg"<<endl;
     }
 
-// test to see if factorial function works
-    //cout<< factorial <<endl;
-
-    //uDS = (factorial/m)
-    cout<<"The factorial of "<< n <<" is "<< factorial<<endl;
-
-    unsigned long long uRI = (factorial % m);
-
-    if (uRI == 0){
-        cout<< "Yes " << m << " goes into " << n <<"!"<<endl;
+    if(userValue <= 4){
+        cout << au1();
     }
 
-    else{
-        cout<< "No " << m << " doesn't go into " << n <<"!" <<endl;
+    else if(userValue > 4 || userValue <=11 ){
+        cout << au2();
     }
-
-
 
 
 return 0;
 }
+
+//au1 function is for if userValue entered is 4 or below
+
+int au1(){
+    int au1R = (userValue * 100);
+    return au1R;
+}
+
+// Calculation for anything above 4 kg
+//temporary calculation for now until they are all put in a class so can use the same variables
+
+int au2(){
+    int auL = (userValue - 4 );
+    int cuL = (auL * 65);
+    int total = cuL + (4*100);
+    return total;
+ }
+
+
